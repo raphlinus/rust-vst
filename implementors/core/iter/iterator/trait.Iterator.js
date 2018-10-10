bef29adcd0eb31b656efb3d1fdc951df588912d6
Travis CI User <travis@example.org>
2018-10-10 02:37:32 +0000

    Update documentation

---
+++ implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["vst"] = [{text:"impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"vst/buffer/struct.InputIterator.html\" title=\"struct vst::buffer::InputIterator\">InputIterator</a>&lt;'a, T&gt;",synthetic:false,types:["vst::buffer::InputIterator"]},{text:"impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"vst/buffer/struct.OutputIterator.html\" title=\"struct vst::buffer::OutputIterator\">OutputIterator</a>&lt;'a, T&gt;",synthetic:false,types:["vst::buffer::OutputIterator"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"vst/api/struct.EventIterator.html\" title=\"struct vst::api::EventIterator\">EventIterator</a>&lt;'a&gt;",synthetic:false,types:["vst::api::EventIterator"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
